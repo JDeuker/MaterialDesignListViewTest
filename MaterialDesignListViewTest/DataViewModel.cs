db0f2749e4c0e41f63162b06d60532ab33fdd287
JDeuker <Jandeuker@yahoo.de>
2019-04-04 14:48:03 +0200

    Add project files.

---
+++ MaterialDesignListViewTest/DataViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialDesignListViewTest
+{
+    class DataViewModel
+    {
+        public string Name { get; set; }
+        public string GroupName { get; set; }
+
+        public DataViewModel(string name, string groupName)
+        {
+            Name = name;
+            GroupName = groupName;
+        }
+    }
+}
---
+++ MaterialDesignListViewTest/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MaterialDesignListViewTest
+{
+    class MainWindowViewModel
+    {
+        private ObservableCollection<DataViewModel> Data { get; set; }
+
+        public ICollectionView DataView { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            this.Data = new ObservableCollection<DataViewModel>();
+            this.DataView = CollectionViewSource.GetDefaultView(this.Data);
+            this.DataView.SortDescriptions.Clear();
+            this.DataView.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Descending));
+            this.DataView.GroupDescriptions.Clear();
+            this.DataView.GroupDescriptions.Add(new PropertyGroupDescription("GroupName"));
+
+            InitTestData();
+        }
+
+        private void InitTestData()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                for (int j = 0; j <= 10; j++)
+                {
+                    this.Data.Add(new DataViewModel($"Name{j}", $"Group{i}"));
+                }
+            }
+        }
+    }
+}
